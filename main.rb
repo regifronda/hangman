54d99a70521c70f291ed7991c36d14bd4a1146b9
Regi Fronda <regisfronda@gmail.com>
2021-07-07 14:59:48 -0700

    Add choose_word method

--- main.rb
+++ main.rb
@@ -1,6 +1,17 @@
+require 'pry'
 module Hangman
   class Game
+    def choose_word
+      words = []
+      File.open("5desk.txt", "r") do |file|
+        file.each_line do |line|
+          words << line.delete("\n").delete("\r")
+        end
+      end
+      words.select {|word| word.length.between?(5, 12)}.sample
+    end
   end
 end
 
 include Hangman
+bruh = Game.new 
