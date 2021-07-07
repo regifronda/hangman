e90f2d1bac8feeb84ec2e0982a0c410688ae0289
Regi Fronda <regisfronda@gmail.com>
2021-07-07 15:47:30 -0700

    Remove extra * when showing @hidden_word

--- main.rb
+++ main.rb
@@ -1,6 +1,12 @@
 require 'pry'
 module Hangman
   class Game
+    attr_accessor :word, :hidden_word
+
+    def initialize
+      @word = choose_word
+    end
+
     def choose_word
       words = []
       File.open("5desk.txt", "r") do |file|
@@ -8,10 +14,15 @@ module Hangman
           words << line.delete("\n").delete("\r")
         end
       end
-      words.select {|word| word.length.between?(5, 12)}.sample
+      words.select {|word| word.length.between?(5, 12)}.sample.strip
+    end
+    def hide_word
+      @hidden_word = "*" * @word.size 
     end
   end
 end
 
 include Hangman
 bruh = Game.new 
+puts big = bruh.choose_word
+puts chungus = bruh.hide_word
