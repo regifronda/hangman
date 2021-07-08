62591940b0b94b01960d8ee6c500228d85e941ce
Regi Fronda <regisfronda@gmail.com>
2021-07-07 17:03:03 -0700

    Add check_lose method

--- main.rb
+++ main.rb
@@ -5,19 +5,31 @@ module Hangman
 
     def initialize
       @word = choose_word
-      p @word.length
       p @word
       @right_guesses = Array.new(@word.length, "_")
       p @right_guesses
       @wrong_guesses = Array.new
       p @wrong_guesses
+      p @wrong_guesses.length
     end
 
+    def check_lose
+      if @wrong_guesses.length == 6
+        "Reached 6 guesses! You lose!"
+        true
+      else
+        false
+      end
+    end
     def choose_word
-      File.readlines("5desk.txt").sample.strip()
+      words = Array.new
+      File.open("5desk.txt", "r") do |file|
+        file.each_line do |line|
+          words << line.delete("\n").delete("\r")
+        end
+      end
+      words.select {|word| word.length.between?(5, 12)}.sample
     end
-
-    
   end
 
   class Player
